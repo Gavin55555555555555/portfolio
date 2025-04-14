8b9a55a1164b5f5a73aafc1633bc1da4c5ac1e33
Gavin55555555555555 <gawu@ucsd.edu>
2025-04-13 18:44:23 -0700

    Update global.js

--- global.js
+++ global.js
@@ -12,35 +12,30 @@ let pages = [
   {url: 'https://github.com/Gavin55555555555555', title: 'GitHub'}
 ];
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+  
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  
+  if (a.host !== location.host) {
+    a.setAttribute("target", "_blank");
+  }
+  
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
   nav.append(a);
-}
-
-
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/website/";         // GitHub Pages repo name
-
-  if (!url.startsWith('http')) {
-  url = BASE_PATH + url;
-}
-
-if (!url.startsWith('http')) {
-  url = BASE_PATH + url;
-}
-
-if (a.host === location.host && a.pathname === location.pathname) {
-  a.classList.add('current');
-}
-
-if (a.host !== location.host) {
-  a.setAttribute("target", "_blank");
 }
