52643a25076d466891f5aa46b11ad85e71828904
Gavin55555555555555 <gawu@ucsd.edu>
2025-04-13 18:18:55 -0700

    Update global.js

--- global.js
+++ global.js
@@ -20,7 +20,10 @@ for (let p of pages) {
   // next step: create link and add it to nav
 }
 
-nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+let a = document.createElement('a');
+a.href = url;
+a.textContent = title;
+nav.append(a);
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
@@ -32,4 +35,12 @@ const BASE_PATH = (location.hostname === "localhost" || location.hostname === "1
 
 if (!url.startsWith('http')) {
   url = BASE_PATH + url;
+}
+
+if (a.host === location.host && a.pathname === location.pathname) {
+  a.classList.add('current');
+}
+
+if (a.host !== location.host) {
+  a.setAttribute("target", "_blank");
 }
