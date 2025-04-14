40c36df126d57d89cf72bb7a5504f192651077e9
Gavin55555555555555 <gawu@ucsd.edu>
2025-04-13 18:10:30 -0700

    Remove nav

--- global.js
+++ global.js
@@ -3,3 +3,14 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'projects/', title: 'Contact' }
+  { url: 'projects/', title: 'Resume' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
