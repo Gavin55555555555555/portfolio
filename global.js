7050368c9363c93dad91a721cfdaf6a0633ea43d
Gavin55555555555555 <gawu@ucsd.edu>
2025-04-16 17:20:26 -0700

    Update global.js

--- global.js
+++ global.js
@@ -54,3 +54,4 @@ document.body.insertAdjacentHTML(
 select.addEventListener('input', function (event) {
   console.log('color scheme changed to', event.target.value);
 });
+document.documentElement.style.setProperty('color-scheme', event.target.value);
